559a672a1600a2bc9cffde7df7ca31c735571d1c
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-12-27 08:57:44 -0300

    Create calculadora_de_odds.py

---
+++ calculadora_de_odds.py
@@ -0,0 +1,112 @@
+import streamlit as st
+
+def calcular_odd_justa(vitorias_casa, empates, derrotas):
+    total_partidas = vitorias_casa + empates + derrotas
+
+    # Calcula as probabilidades
+    prob_vitoria_casa = vitorias_casa / total_partidas
+    prob_empate = empates / total_partidas
+    prob_vitoria_visitante = derrotas / total_partidas
+
+    # Calcula as odds justas para Match Odds
+    odd_vitoria_casa = 1 / prob_vitoria_casa
+    odd_empate = 1 / prob_empate
+    odd_vitoria_visitante = 1 / prob_vitoria_visitante
+
+    # Calcula a linha de handicap (por exemplo, linha 0)
+    linha_handicap = (vitorias_casa - derrotas) / total_partidas
+
+    # Calcula as odds justas para Handicap Asiático (linha 0)
+    odd_vitoria_casa_handicap = 1 / (prob_vitoria_casa + linha_handicap)
+    odd_vitoria_visitante_handicap = 1 / (prob_vitoria_visitante - linha_handicap)
+
+    return {
+        "Match Odds": {
+            "Odd Vitória da Casa": odd_vitoria_casa,
+            "Odd Empate": odd_empate,
+            "Odd Vitória Visitante": odd_vitoria_visitante,
+        },
+        "Handicap Asiático": {
+            "Linha de Handicap": linha_handicap,
+            "Odd Vitória da Casa (Handicap)": odd_vitoria_casa_handicap,
+            "Odd Vitória Visitante (Handicap)": odd_vitoria_visitante_handicap,
+        },
+    }
+
+# Interface do Streamlit
+st.title("import streamlit as st
+
+def calcular_odd_justa(vitorias_casa, empates, derrotas):
+    total_partidas = vitorias_casa + empates + derrotas
+
+    # Calcula as probabilidades
+    prob_vitoria_casa = vitorias_casa / total_partidas
+    prob_empate = empates / total_partidas
+    prob_vitoria_visitante = derrotas / total_partidas
+
+    # Calcula as odds justas para Match Odds
+    odd_vitoria_casa = 1 / prob_vitoria_casa
+    odd_empate = 1 / prob_empate
+    odd_vitoria_visitante = 1 / prob_vitoria_visitante
+
+    # Calcula a linha de handicap (por exemplo, linha 0)
+    linha_handicap = (vitorias_casa - derrotas) / total_partidas
+
+    # Calcula as odds justas para Handicap Asiático (linha 0)
+    odd_vitoria_casa_handicap = 1 / (prob_vitoria_casa + linha_handicap)
+    odd_vitoria_visitante_handicap = 1 / (prob_vitoria_visitante - linha_handicap)
+
+    return {
+        "Match Odds": {
+            "Odd Vitória da Casa": odd_vitoria_casa,
+            "Odd Empate": odd_empate,
+            "Odd Vitória Visitante": odd_vitoria_visitante,
+        },
+        "Handicap Asiático": {
+            "Linha de Handicap": linha_handicap,
+            "Odd Vitória da Casa (Handicap)": odd_vitoria_casa_handicap,
+            "Odd Vitória Visitante (Handicap)": odd_vitoria_visitante_handicap,
+        },
+    }
+
+# Interface do Streamlit
+st.title("Calculadora de Odds")
+
+# Entradas do usuário
+vitorias_casa = st.slider("Número de Vitórias da Casa:", min_value=0, max_value=10, value=5)
+empates = st.slider("Número de Empates:", min_value=0, max_value=10, value=3)
+derrotas = st.slider("Número de Derrotas:", min_value=0, max_value=10, value=2)
+
+# Calcula as odds justas
+odds_justas = calcular_odd_justa(vitorias_casa, empates, derrotas)
+
+# Exibe as odds decimais
+st.write("### Match Odds:")
+st.write("Odd Vitória da Casa:", round(odds_justas["Match Odds"]["Odd Vitória da Casa"], 2))
+st.write("Odd Empate:", round(odds_justas["Match Odds"]["Odd Empate"], 2))
+st.write("Odd Vitória Visitante:", round(odds_justas["Match Odds"]["Odd Vitória Visitante"], 2))
+
+st.write("### Handicap Asiático (Linha 0):")
+st.write("Linha de Handicap:", round(odds_justas["Handicap Asiático"]["Linha de Handicap"], 2))
+st.write("Odd Vitória da Casa (Handicap):", round(odds_justas["Handicap Asiático"]["Odd Vitória da Casa (Handicap)"], 2))
+st.write("Odd Vitória Visitante (Handicap):", round(odds_justas["Handicap Asiático"]["Odd Vitória Visitante (Handicap)"], 2))
+")
+
+# Entradas do usuário
+vitorias_casa = st.slider("Número de Vitórias da Casa:", min_value=0, max_value=10, value=5)
+empates = st.slider("Número de Empates:", min_value=0, max_value=10, value=3)
+derrotas = st.slider("Número de Derrotas:", min_value=0, max_value=10, value=2)
+
+# Calcula as odds justas
+odds_justas = calcular_odd_justa(vitorias_casa, empates, derrotas)
+
+# Exibe as odds decimais
+st.write("### Match Odds:")
+st.write("Odd Vitória da Casa:", round(odds_justas["Match Odds"]["Odd Vitória da Casa"], 2))
+st.write("Odd Empate:", round(odds_justas["Match Odds"]["Odd Empate"], 2))
+st.write("Odd Vitória Visitante:", round(odds_justas["Match Odds"]["Odd Vitória Visitante"], 2))
+
+st.write("### Handicap Asiático (Linha 0):")
+st.write("Linha de Handicap:", round(odds_justas["Handicap Asiático"]["Linha de Handicap"], 2))
+st.write("Odd Vitória da Casa (Handicap):", round(odds_justas["Handicap Asiático"]["Odd Vitória da Casa (Handicap)"], 2))
+st.write("Odd Vitória Visitante (Handicap):", round(odds_justas["Handicap Asiático"]["Odd Vitória Visitante (Handicap)"], 2))
